154724daa29a2b409bd06908dd25827f129d20a0
Sam Doyle <sdoyle@teamworks.com>
2019-11-22 15:53:45 -0500

    revert previous android changes

--- android/src/main/java/com/reactlibrary/LaunchdarklyReactNativeClientModule.java
+++ android/src/main/java/com/reactlibrary/LaunchdarklyReactNativeClientModule.java
@@ -1,4 +1,4 @@
-package com.LaunchdarklyReactNativeClient;
+package com.reactlibrary;
 
 import android.app.Application;
 import android.net.Uri;
--- android/src/main/java/com/reactlibrary/LaunchdarklyReactNativeClientPackage.java
+++ android/src/main/java/com/reactlibrary/LaunchdarklyReactNativeClientPackage.java
@@ -1,5 +1,5 @@
 
-package com.LaunchdarklyReactNativeClient;
+package com.reactlibrary;
 
 import java.util.Arrays;
 import java.util.Collections;
