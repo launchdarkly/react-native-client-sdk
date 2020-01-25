0ccd03eb1ac894dd1aae9bdf93b7084d1976cda7
Ben Woskow <48036130+bwoskow-ld@users.noreply.github.com>
2020-01-24 16:58:46 -0800

    also update package.json (#33)

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -2,3 +2,6 @@
 
 sed  -i.bak "s/\( *\)s\.version\( *\)=\( *\)\".*\"/\1s\.version\2=\3\"${LD_RELEASE_VERSION}\"/" ios/LaunchdarklyReactNativeClient.podspec
 rm -f ios/LaunchdarklyReactNativeClient.podspec.bak
+
+sed  -i.bak "s/\( *\)\"version\"\( *\):\( *\)\".*\"/\1\"version\"\2:\3\"${LD_RELEASE_VERSION}\"/" package.json
+rm -f package.json.bak
