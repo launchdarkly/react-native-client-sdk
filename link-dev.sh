48b8f1033d2ed56e9d45f1f58756d9a94567fbcc
Yusinto Ngadiman <yusinto@gmail.com>
2023-01-11 13:59:17 -0800

    Initial commit

---
+++ link-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "===== Installing all dependencies..."
+yarn
+
+declare -a examples=(ManualTestApp)
+
+for example in "${examples[@]}"
+do
+  echo "===== Linking to $example"
+  mkdir -p ${example}/node_modules
+  rm -rf ${example}/node_modules/launchdarkly-react-native-client-sdk
+  mkdir -p ${example}/node_modules/launchdarkly-react-native-client-sdk/node_modules
+  cp package.json ${example}/node_modules/launchdarkly-react-native-client-sdk/package.json
+  cp index.js ${example}/node_modules/launchdarkly-react-native-client-sdk/index.js
+  cp -r node_modules/ ${example}/node_modules/launchdarkly-react-native-client-sdk/node_modules/
+  cp -r ios/ ${example}/node_modules/launchdarkly-react-native-client-sdk/ios
+  cp -r android/ ${example}/node_modules/launchdarkly-react-native-client-sdk/android
+done
