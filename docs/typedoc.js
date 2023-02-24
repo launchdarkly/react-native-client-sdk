5691f78311a33c6fd73c671391f7f2ebc60fd028
Yusinto Ngadiman <yusinto@gmail.com>
2023-02-24 13:21:08 -0800

    Update typedoc.js

--- docs/typedoc.js
+++ docs/typedoc.js
@@ -1,7 +1,7 @@
 module.exports = {
   out: '/tmp/project-releaser/project/docs/build/html',
   exclude: ['**/node_modules/**', 'test-types.ts'],
-  name: 'LaunchDarkly Client-Side SDK for React Native (6.0.0)',
+  name: 'LaunchDarkly Client-Side SDK for React Native (7.0.0)',
   readme: 'none', // don't add a home page with a copy of README.md
   entryPoints: '/tmp/project-releaser/project/index.d.ts',
   entryPointStrategy: 'expand',
